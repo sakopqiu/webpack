8ca5c9d16b9680225bbe33b170d03de99ddc6a90
sakop <sakopqiu@gmail.com>
2017-12-29 18:36:40 +0800

    aa

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,27 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
     entry: {
-        app: './src/index.js',
-        print: './src/print.js'
-    },
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: "/js/react"
+        app: './src/index.js'
     },
     devtool: 'inline-source-map',
-    // devServer: {
-    //     contentBase: './dist'
-    // },
+    devServer: {
+        contentBase: './dist',
+        hot: true
+    },
     plugins: [
+        new CleanWebpackPlugin(['dist']),
         new HtmlWebpackPlugin({
-            title: 'Output Management'
+            title: 'Hot Module Replacement'
         }),
-        new CleanWebpackPlugin(["dist"])
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin()
     ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    }
 };
